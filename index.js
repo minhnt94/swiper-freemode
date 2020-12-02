701b2f03cdbb0d693d7a2b79579e2044872d930b
minhnt <minhnt94@gmail.com>
2020-12-02 22:37:13 +0700

    add demo for freemode

---
+++ index.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+  grabCursor: true,
+  loop: true
+  // autoplay: {
+  //   delay: 0,
+  // },
+  // speed: 5000
+});
